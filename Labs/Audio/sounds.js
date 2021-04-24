619735e22baa91da8b8a188e6649ea3cdcc1edfc
linchristy <christy.lin92@myhunter.cuny.edu>
2021-04-24 18:46:53 -0400

    Add files

---
+++ Labs/Audio/sounds.js
@@ -0,0 +1,42 @@
+var rain = document.getElementById("rain-audio");
+var storm_hurricane = document.getElementById("storm-hurricane-audio");
+var water_rain = document.getElementById("water-rain-audio");
+var storm_thunder = document.getElementById("storm-thunder-audio");
+var wind_howl = document.getElementById("wind-howl-audio");
+
+function playRain(){
+    rain.play();
+}
+function pauseRain(){
+    rain.pause();
+}
+
+function playStormHurricane(){
+    storm_hurricane.play();
+}
+function pauseStormHurricane(){
+    storm_hurricane.pause();
+}
+
+function playWaterRain(){
+    water_rain.play();
+}
+function pauseWaterRain(){
+    water_rain.pause();
+}
+
+function playStormThunder(){
+    storm_thunder.play();
+}
+
+function pauseStormThunder(){
+    storm_thunder.pause();
+}
+
+function playWindHowl(){
+    wind_howl.play();
+}
+
+function pauseWindHowl(){
+    wind_howl.pause();
+}
